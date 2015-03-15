e03bf26decc8dd66dc2102429b0fe84ab2ee1569
bstaykov <boycho.staykov@gmail.com>
2015-03-15 15:00:20 +0200

    Add Homework

---
+++ Defining-Classes-Part-One/01.MobilePhoneDeviceClasses/BatteryType.cs
@@ -0,0 +1,7 @@
+﻿namespace _01.MobilePhoneDeviceClasses
+{
+    public enum BatteryType
+    {
+        LiIon, NiMH, NiCd, UNKNOWN
+    }
+}
---
+++ Defining-Classes-Part-One/01.MobilePhoneDeviceClasses/Display.cs
@@ -0,0 +1,63 @@
+﻿namespace _01.MobilePhoneDeviceClasses
+{
+    using System;
+
+    public class Display
+    {
+        private int size;
+        private int numberOfColors;
+
+        public Display(int displaySize, int numberOfColors)
+        {
+            this.Size = displaySize;
+            this.NumberOfColors = numberOfColors;
+        }
+
+        public Display()
+        {
+            this.Size = 0;
+            this.NumberOfColors = 0;
+        }
+
+        public int Size 
+        {
+            get
+            {
+                return this.size;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Display size is out of range!");
+                }
+
+                this.size = value;
+            }
+        }
+
+        public int NumberOfColors
+        {
+            get
+            {
+                return this.numberOfColors;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Display size is out of range!");
+                }
+
+                this.numberOfColors = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("===Display===\nSize: {0}\nNumber of colors: {1}", this.Size, this.NumberOfColors);
+        }
+    }
+}
