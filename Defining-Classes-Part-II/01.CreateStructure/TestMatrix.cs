586bdc70a3c6e27b91bc38ab5f3c08141216c430
bstaykov <boycho.staykov@gmail.com>
2015-03-17 23:15:36 +0200

    Defining classes part 2

---
+++ Defining-Classes-Part-II/01.CreateStructure/TestMatrix.cs
@@ -0,0 +1,34 @@
+﻿namespace _01.CreateStructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class TestMatrixClass
+    {
+        // static void Main()
+        // {
+        // double[,] first = { { 0, 2, 3, 4.2 }, { 1, 2, 3, 4 }, { 1, 2, 3, 4 }, { 1, 2, 3, 4 } };
+        // double[,] second = { { 1, 2, 3, 4 }, { 1, 2, 3, 4 }, { 1, 2, 3, 4 }, { 1, 0, 3, 4 } };
+
+        // Matrix<double> arrFirst = new Matrix<double>(first);
+        // Matrix<double> arrSecond = new Matrix<double>(second);
+
+        // //checking if contain zero, return true, if not, return false if there is zero
+        // if (arrFirst)
+        // {
+        //     Console.WriteLine("There is no zero inside");
+        // }
+        // else Console.WriteLine("There is at least one zero inside\n");
+        // //-----------
+        // Console.WriteLine("Sum of the two matrices");
+        // Console.WriteLine(arrFirst + arrSecond);
+        // Console.WriteLine("Substraction of the two matrices");
+        // Console.WriteLine(arrFirst - arrSecond);
+        // Console.WriteLine("Multiplication of the two matrices");
+        // Console.WriteLine(arrFirst * arrSecond);
+        // }
+    }
+}
---
+++ Defining-Classes-Part-II/Profiler/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Profiler
+{
+    using System;
+    using System.IO;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            UserProfile profile = new UserProfile();
+            profile.FirstName = "Ivailo";
+            profile.LastName = "Kenov";
+            profile.DateOfBirth = new DateTime(2001, 1, 1);
+            profile.Age = 25;
+            profile.Description = "'using_atributes'";
+
+            Save(profile);
+            UserProfile loadedProfiele = Load();
+        }
+
+        public static void Save(UserProfile profile)
+        {
+            StreamWriter writer = new StreamWriter("data.txt");
+
+            using (writer)
+            {
+                Type profileType = typeof(UserProfile);
+                var profileProperties = profileType.GetProperties();
+
+                foreach (var property in profileProperties)
+                {
+                    var attribute = property.GetCustomAttributes(typeof(Save), false);
+
+                    if (attribute.Length >= 1)
+                    {
+                        writer.WriteLine(property.Name + " " + property.GetValue(profile)); 
+                    }
+                }
+            }
+        }
+
+        public static UserProfile Load()
+        {
+            StreamReader reader = new StreamReader("data.txt");
+
+            UserProfile profile = new UserProfile();
+
+            using (reader)
+            {
+                Type profileType = typeof(UserProfile);
+                string currentData = reader.ReadLine();
+
+                while (!string.IsNullOrEmpty(currentData))
+                {
+                    string[] values = currentData.Split(' ');
+
+                    string propertieName = values[0];
+                    string propertyValue = values[1];
+
+                    var property = profileType.GetProperty(propertieName);
+                    property.SetValue(profile, propertyValue);
+
+                    currentData = reader.ReadLine();
+
+                    // object convert = Convert.ChangeType(propertyValue, typeof(int));
+                    Console.WriteLine(propertieName + " " + propertyValue);
+                }
+            }
+
+            return profile;
+        }
+    }
+}
